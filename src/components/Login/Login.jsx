0af6230351e8701c5e7fa60db2e5df38e409892d
Lucas <lucasclaudinocc@gmail.com>
2023-08-23 14:55:20 -0300

    Adding postgreSQL custom database parameters

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Login = ({ setRoute }) => {
 
-    function handleClickSign () {
+    function handleClickSign (email, pass) {
+        // if(d){}
         setRoute('main');
     }
 
@@ -12,12 +13,12 @@ const Login = ({ setRoute }) => {
 
   return (
     <div className='flex justify-center items-center w-[100svw] h-[100svh]'>
-        <div className="md:w-[30%] md:h-[50%] flex flex-col justify-center items-center gap-16 rounded-2xl" style={{background: 'rgba(0, 0, 0, 0.5)', border: '1px'}}>
-            <div className="flex flex-col gap-5">
-                <p className='w-[75%] text-lg mx-auto'>Username:<input type="text" className='w-full' placeholder='Ex: example256' /></p>
-                <p className='w-[75%] text-lg mx-auto'>Password:<input type="password" className='w-full' placeholder='Ex: mypAsSword@32' /></p>
+        <div className="md:w-[30%] sm:w-[60%] md:h-[50%] flex flex-col justify-center items-center gap-16 rounded-2xl" style={{background: 'rgba(0, 0, 0, 0.7)', border: '1px'}}>
+            <div className="flex flex-col gap-5 justify-center align-center pt-10">
+                <p className='w-[75%] text-lg mx-auto text-center'>Email<input type="text" className='w-full bg-slate-400' /></p>
+                <p className='w-[75%] text-lg mx-auto text-center'>Password<input type="password" className='w-full bg-slate-400' /></p>
             </div>
-            <div className="buttons flex justify-around items-center gap-10">
+            <div className="buttons flex justify-around items-center gap-10 pb-10">
                 <button className='bg-pink-700 px-5 py-2 rounded-2xl font-extrabold capitalize hover:bg-white hover:text-black' onClick={() => setRoute('main')} >Sign In</button>
                 <button className='bg-pink-400 px-5 py-2 rounded-2xl font-extrabold capitalize hover:bg-white hover:text-black' onClick={() => setRoute('register')} >Register</button>
             </div>
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -24,13 +24,13 @@ const Register = ({ setRoute }) => {
 
   return (
     <div className='flex justify-center items-center w-[100svw] h-[100svh]'>
-        <div className="md:w-[30%] md:h-[50%] flex flex-col justify-center items-center gap-16 rounded-2xl" style={{background: 'rgba(0, 0, 0, 0.5)', border: '1px'}}>
-            <div className="flex flex-col gap-5">
-                <p className='w-[75%] text-lg mx-auto'>Name:<input type="text" className='w-full' placeholder='Ex: Vladmir Ilitch' onChange={e => setName(e.target.value)} /></p>
-                <p className='w-[75%] text-lg mx-auto'>Email:<input type="text" className='w-full' placeholder='Ex: yourname@example.com' onChange={e => setEmail(e.target.value)} /></p>
-                <p className='w-[75%] text-lg mx-auto'>Password:<input type="password" className='w-full' placeholder='Ex: mypAsSword@32' onChange={e => setPassword(e.target.value)} /></p>
+        <div className="md:w-[30%] md:h-[50%] flex flex-col justify-center items-center gap-16 rounded-2xl" style={{background: 'rgba(0, 0, 0, 0.7)', border: '1px'}}>
+            <div className="flex flex-col gap-5 pt-10">
+                <p className='w-[75%] text-lg mx-auto text-center'>Name<input type="text" className='w-full bg-slate-400' onChange={e => setName(e.target.value)} /></p>
+                <p className='w-[75%] text-lg mx-auto text-center'>Email<input type="text" className='w-full bg-slate-400' onChange={e => setEmail(e.target.value)} /></p>
+                <p className='w-[75%] text-lg mx-auto text-center'>Password<input type="password" className='w-full bg-slate-400'  onChange={e => setPassword(e.target.value)} /></p>
             </div>
-            <div className="buttons flex justify-around items-center gap-10">
+            <div className="buttons flex justify-around items-center gap-10 pb-10">
                 <button className='bg-pink-400 px-5 py-2 rounded-2xl font-extrabold capitalize hover:bg-white hover:text-black' onClick={handleClick}>Register</button>
                 <button className='bg-pink-800 px-5 py-2 rounded-2xl font-extrabold capitalize hover:bg-white hover:text-black' >Cancel</button>
             </div>
